108bf5a3d8bace5a3f43406be9e0e2e876dcd490
ogddhm <gduranapubli@gmail.com>
2020-02-22 14:18:10 +0100

    editado boton en c#

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -23,6 +23,34 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
+
+
+
+
+            /*Grid miGrid = new Grid();
+            this.Content = miGrid;
+            Button miBoton = new Button();
+            miBoton.Width = 200;
+            miBoton.Height = 100;
+            miBoton.Background = Brushes.Yellow;
+            WrapPanel miWrap = new WrapPanel();
+
+            TextBlock txt1 = new TextBlock();
+            txt1.Text = "hola";
+            miWrap.Children.Add(txt1);
+
+            TextBlock txt2 = new TextBlock();
+            txt2.Text = "Click";
+            txt2.Foreground = Brushes.Red;
+            miWrap.Children.Add(txt2);
+
+            TextBlock txt3 = new TextBlock();
+            txt3.Text = "Bye";
+            miWrap.Children.Add(txt3);
+
+            miBoton.Content=miWrap;
+            miGrid.Children.Add(miBoton);*/
+
         }
     }
 }
